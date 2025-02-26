fa84e89ef2f56bc6eaf3aee818955cdd39152563
choutianxius <choutianxius@gmail.com>
2025-02-26 15:45:36 -0600

    Naming

--- src/main/java/com/choutianxius/test_spring_auth/TestSpringAuthApplication.java
+++ src/main/java/com/choutianxius/test_spring_auth/TestSpringAuthApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestAuthApplication {
+public class TestSpringAuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestAuthApplication.class, args);
+		SpringApplication.run(TestSpringAuthApplication.class, args);
 	}
 
 }
